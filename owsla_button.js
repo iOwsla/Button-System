4cd9c935135002d2366de80d5f140d13da274390
Owsla <81816429+iOwsla@users.noreply.github.com>
2021-06-13 16:33:28 +0300

    Update owsla_button.js

--- owsla_button.js
+++ owsla_button.js
@@ -12,19 +12,9 @@ let config = {
 
 client.on("message", async (message) => {
     if (message.content === "!button" && message.author.id === config.botOwner) return; // BOT SAHİBİ ID' GİRİNİZ VE BU KOMUTU SADECE 1 DEFALIĞINA 1 KANALDA KULLANINIZ!
-    let vk = new disbut.MessageButton()
-        .setStyle('green')
-        .setLabel('Vampir Köylü!')
-        .setID('vk')
-    let dc = new disbut.MessageButton()
-        .setStyle('red')
-        .setLabel('Doğruluk / Cesaretlik!')
-        .setID('dc')
-    let grt = new disbut.MessageButton()
-        .setStyle("gray")
-        .setLabel('Gartic!')
-        .setID('gartic')
-
+    let vk = new disbut.MessageButton().setStyle('green').setLabel('Vampir Köylü!').setID('vk')
+    let dc = new disbut.MessageButton().setStyle('red').setLabel('Doğruluk / Cesaretlik!').setID('dc')
+    let grt = new disbut.MessageButton().setStyle("gray").setLabel('Gartic!').setID('gartic')
     message.channel.send('Aşağıdaki menüden kendinize oyun seçebilirsiniz. Bir oyunun rolünü almak için o butona tıklayın.', {
         buttons: [vk, dc, grt]
     });
@@ -33,36 +23,24 @@ client.on("message", async (message) => {
 client.on('clickButton', async (button) => {
     if (button.id === 'vk') {
         if (button.clicker.member.roles.cache.get(config.vk)) {
-            await button.clicker.member.roles.remove(config.vk)
-            await button.think(true);
-            await button.reply.edit("Vampir Köylü rolü üzerinizden alındı.")
+            await button.clicker.member.roles.remove(config.vk);await button.think(true);await button.reply.edit("Vampir Köylü rolü üzerinizden alındı.")
         } else {
-            await button.clicker.member.roles.add(config.vk)
-            await button.think(true);
-            await button.reply.edit("Vampir Köylü rolü üzerinize verildi.")
+            await button.clicker.member.roles.add(config.vk);await button.think(true);await button.reply.edit("Vampir Köylü rolü üzerinize verildi.")
         }
     }
     if (button.id === 'dc') {
         if (button.clicker.member.roles.cache.get(config.dc)) {
-            await button.clicker.member.roles.remove(config.dc)
-            await button.think(true);
-            await button.reply.edit("Doğruluk Cesaret rolü üzerinizden alındı.")
+            await button.clicker.member.roles.remove(config.dc);await button.think(true);await button.reply.edit("Doğruluk Cesaret rolü üzerinizden alındı.")
         } else {
-            await button.clicker.member.roles.add(config.dc)
-            await button.think(true);
-            await button.reply.edit("Doğruluk Cesaret rolü üzerinize verildi.")
+            await button.clicker.member.roles.add(config.dc);await button.think(true);await button.reply.edit("Doğruluk Cesaret rolü üzerinize verildi.")
         }
 
     }
     if (button.id === 'gartic') {
         if (button.clicker.member.roles.cache.get(config.gartic)) {
-            await button.clicker.member.roles.remove(config.gartic)
-            await button.think(true);
-            await button.reply.edit("Gartic.İO rolü üzerinizden alındı.")
+            await button.clicker.member.roles.remove(config.gartic);await button.think(true);await button.reply.edit("Gartic.İO rolü üzerinizden alındı.")
         } else {
-            await button.clicker.member.roles.add(config.gartic)
-            await button.think(true);
-            await button.reply.edit("Gartic rolü üzerinize verildi.")
+            await button.clicker.member.roles.add(config.gartic);await button.think(true);await button.reply.edit("Gartic rolü üzerinize verildi.")
         }
 
     }
